b348fa0b876eeb6b2ebc29b16c731c3f0967d805
S M Mohi Us Sunnat <suncha629@gmail.com>
2019-08-28 22:06:30 +0600

    Initial commit

---
+++ app/src/main/java/dev/sunnat629/crpermissions/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.sunnat629.crpermissions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
